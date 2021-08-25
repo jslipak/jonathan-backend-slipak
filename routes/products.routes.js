602db11a45a9ebd27b3f8ddc33f70ffe516e5b53
jslipak <seriedgigital@gmail.com>
2021-08-25 00:03:14 -0300

    second commit pug

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -9,10 +9,8 @@ route.get('/new', (req, res) => {
 route.get('/edit/:id', async (req, res) => {
   const id = req.params.id;
   const fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
-  console.log(fullUrl)
-  console.log(id)
   const temp = await Producto.getDataId(id);
-  console.log('porque underfined:',temp);
+  console.log('porque underfined:', temp);
   return res.render('edit', { layout: 'edit', edit: temp });
 });
 
