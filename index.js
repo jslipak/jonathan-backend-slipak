d30e901648c8240e2c22cc11cde03ed2f2695173
jslipak <seriedgigital@gmail.com>
2021-08-31 04:59:58 -0300

    before to split the socket.io

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 const puerto = 8080;
 const Routes = require('./routes');
 const morgan = require('morgan');
@@ -7,24 +9,35 @@ const handlebars = require('express-handlebars');
 
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(express.static('public'));
 app.use('/api', Routes);
+
 app.engine(
-  'hbs',
-  handlebars({
-    extname: 'hbs',
-    layoutsDir: __dirname + '/views/layouts',
-    partialsDir: __dirname + '/views/partials',
-    defaultLayout: 'index',
-  }),
+    'hbs',
+    handlebars({
+        extname: 'hbs',
+        layoutsDir: __dirname + '/views/layouts',
+        partialsDir: __dirname + '/views/partials',
+        defaultLayout: 'index',
+    }),
 );
+
+io.on('connection', (socket) => {
+console.log(`Usuario conectado: ${socket.id}`);
+socket.on("mensaje", (arg) => {
+    console.log(arg);
+    io.emit('mensaje', arg)
+});
+
+});
 
 app.set('views', './views/layouts');
 app.set('view engine', 'hbs');
-
-const server = app.listen(puerto, () => {
-  console.log(`Servidor inicializado en el puerto ${server.address().port}`);
+const server = http.listen(puerto, () => {
+    console.log(`Servidor inicializado en el puerto ${server.address().port}`);
 });
 
 server.on('error', (err) => console.log(`Error en servidor ${err}`));
