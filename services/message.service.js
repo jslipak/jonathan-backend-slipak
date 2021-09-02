94eeb7108a79453312fb06af4d8a47587008a15f
jslipak <seriedgigital@gmail.com>
2021-09-02 13:03:01 -0300

    before go to dentist

---
+++ services/message.service.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const msg = JSON.parse(fs.readFileSync('./messages.json', (encoding = 'utf8')));
+
+class Mensaje {
+  async create(obj) {
+    msg.push(obj);
+    await fs.promises.writeFile('./messages.json', JSON.stringify(msg));
+    console.log('se a agregado un nuevo mensaje:', obj);
+  }
+}
+
+module.exports = new Mensaje();
