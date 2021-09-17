8f267f3245eac6e643acb8998a7b71dafb252afe
jslipak <seriedgigital@gmail.com>
2021-09-17 00:59:11 -0300

    entrega 2 sql v2

--- config/sqlite.config.js
+++ config/sqlite.config.js
@@ -1,6 +1,6 @@
 const knex = require('knex');
-const config = require('../knexfile');
-//const config = require('../knexfile.mysql');
+//const config = require('../knexfile');
+const config = require('../knexfile.mysql');
 
 const db = knex(config.development);
 
--- knexfile.mysql.js
+++ knexfile.mysql.js
@@ -2,13 +2,12 @@
 
 module.exports = {
   development: {
-    client: 'mysql',
+    client: 'mysql2',
     connection: {
       host: '127.0.0.1',
       port: 3306,
-      user: 'your_database_user',
-      password: 'your_database_password',
-      database: 'myapp_te',
+      user: 'root',
+      database: 'test',
     },
     useNullAsDefault: true,
     migrations: {
