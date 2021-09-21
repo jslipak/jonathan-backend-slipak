29896aa798e56b3f434d4b47f075a915524b474a
jslipak <seriedgigital@gmail.com>
2021-09-21 00:08:08 -0300

    fix create order by id

--- services/cart.services.js
+++ services/cart.services.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
 const data = JSON.parse(fs.readFileSync('./ordens.json', (encoding = 'utf8')));
+const product = JSON.parse(
+  fs.readFileSync('./productos.json', (encoding = 'utf8')),
+);
 
 class Carrito {
   getAll(req, res) {
@@ -12,10 +15,17 @@ class Carrito {
   }
   async create(req, res) {
     const insertData = {};
+    const productData = [];
+    const array = req.body;
+    array.forEach((item) => {
+      console.log(item);
+      const addProduct = product.find((x) => x.id == item.id);
+      addProduct.cantidad = item.cantidad;
+      productData.push(addProduct);
+    });
     insertData.id = data.length;
     insertData.timestamp = Date.now();
-    insertData.product = req.body;
-    console.log(req.body);
+    insertData.product = productData;
     data.push(insertData);
     await fs.promises.writeFile('./ordens.json', JSON.stringify(data));
     return res.json({ producto: 'Orden creado' });
