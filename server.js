7484962d257839aaeb10c90a52401f24012a8e0a
jslipak <seriedgigital@gmail.com>
2021-10-02 09:45:15 -0300

    2 octubre

--- server.js
+++ server.js
@@ -1,71 +1,58 @@
-'use strict';
+const express = require('express');
+//import express from 'express';
+//import mongoose from 'mongoose';
+const mongoose = require('mongoose');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const puerto = 8080;
+const Routes = require('./routes');
+//import Routes from './routes';
+//import morgan from 'morgan';
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+//import handlebars from 'express-handlebars';
+//import SocketIO from './services/socket.service';
+const SocketIO = require('socket.io');
 
-var _express = require('express');
-
-var _express2 = _interopRequireDefault(_express);
-
-var _mongoose = require('mongoose');
-
-var _mongoose2 = _interopRequireDefault(_mongoose);
-
-var _routes = require('./routes');
-
-var _routes2 = _interopRequireDefault(_routes);
-
-var _morgan = require('morgan');
-
-var _morgan2 = _interopRequireDefault(_morgan);
-
-var _expressHandlebars = require('express-handlebars');
-
-var _expressHandlebars2 = _interopRequireDefault(_expressHandlebars);
-
-var _socket = require('./services/socket.service');
-
-var _socket2 = _interopRequireDefault(_socket);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-var app = (0, _express2.default)();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var puerto = 8080;
-var NODE_ENV = process.env.NODE_ENV;
-
+const { NODE_ENV } = process.env;
 if (NODE_ENV === 'mongo') {
-  var URL = 'mongodb://root:example@127.0.0.1:27017/ecommerce?authSource=admin';
-  _mongoose2.default.connect(URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }).then(function (data) {
-    return console.log('se ha conectado con mongo');
-  }).catch(function (err) {
-    return console.log(err);
-  });
+  const URL =
+    'mongodb://root:example@127.0.0.1:27017/ecommerce?authSource=admin';
+  mongoose
+    .connect(URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then((data) => console.log('se ha conectado con mongo'))
+    .catch((err) => console.log(err));
 }
-app.use((0, _morgan2.default)('dev'));
-app.use(_express2.default.json());
-app.use(_express2.default.urlencoded({
-  extended: true
-}));
-app.use(_express2.default.static('public'));
-app.use('/api', _routes2.default);
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+);
+app.use(express.static('public'));
+app.use('/api', Routes);
 
-app.engine('hbs', (0, _expressHandlebars2.default)({
-  extname: 'hbs',
-  layoutsDir: __dirname + '/views/layouts',
-  partialsDir: __dirname + '/views/partials',
-  defaultLayout: 'index'
-}));
+app.engine(
+  'hbs',
+  handlebars({
+    extname: 'hbs',
+    layoutsDir: __dirname + '/views/layouts',
+    partialsDir: __dirname + '/views/partials',
+    defaultLayout: 'index',
+  }),
+);
 
-(0, _socket2.default)(io);
+SocketIO(io);
 
 app.set('views', './views/layouts');
 app.set('view engine', 'hbs');
-var server = http.listen(puerto, function () {
-  console.log('Servidor inicializado en el puerto ' + server.address().port);
+const server = http.listen(puerto, () => {
+  console.log(`Servidor inicializado en el puerto ${server.address().port}`);
 });
 
-server.on('error', function (err) {
-  return console.log('Error en servidor ' + err);
-});
+server.on('error', (err) => console.log(`Error en servidor ${err}`));
