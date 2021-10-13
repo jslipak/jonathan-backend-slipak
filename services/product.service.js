343306bb528c45de4c3a8bc41f3e893d946014c5
jslipak <seriedgigital@gmail.com>
2021-10-13 19:50:43 -0300

    front ready 2

--- services/product.service.js
+++ services/product.service.js
@@ -24,8 +24,6 @@ class Producto {
   async getAll(_req, res) {
     const msg = await db.find('messages');
     const temp = JSON.stringify(normalizedMessages(msg));
-    console.log('aca es msg db: ', msg);
-    console.log(temp);
     const productos = await db.find('products');
     productos.map((v) => {
       if ('_id' in v) {
